173aaf86967f4abfc955054dc2650bce91c603d9
annalzrv <annalzrv@mail.ru>
2021-12-13 14:38:54 +0300

    add validator tests

--- FootballApp/FootballApp/Network/Validator.swift
+++ FootballApp/FootballApp/Network/Validator.swift
@@ -9,10 +9,10 @@ import Foundation
 
 class Validator {
     
-    static func isValidEmail(_ email: String) -> Bool {
+    static func isEmailValid(_ email: String) -> Bool {
         
         let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-
+        
         let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
         return emailTest.evaluate(with: email)
     }
